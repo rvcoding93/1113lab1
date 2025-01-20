789ce1a48e3e1d7677ab4a2c16f52ce2b1cb964c
Ronnie Vincenty <ronnievcoding@gmail.com>
2025-01-19 19:58:35 -0500

    fixed formatting on the final return, spacing was off

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,4 +1,12 @@
-#creating the function
+# Program Name: Lab_1.py (use the name the program is saved as)
+# Course: IT1113/Section XXX
+# Student Name: Ronnie Vincenty
+# KSU Email Address: rvince13@students.kennesaw.edu
+# Assignment Lab Number: 1
+# Due Date: 01/19/2025
+# # Purpose: What does the program do(in a few sentences)?
+
+
 def bookstore_checkout():
     print("Welcome to our bookstore!")
     num_books = 6
@@ -11,14 +19,14 @@ def bookstore_checkout():
         books.append((title, price))
 
     print("Purchase Receipt!")
-    print(f"Book Title:"+" " * 15 + "Price:($)")
-    print("-" * 20)
+    print(f"Book Title:"+" " * 5 + "Price:($)")
+    print("-" * 30)
 
     for title, price in books:
         print(f"{title:20}  ${price:.2f}")
         total_price += price
 
-    print("-" * 20)
+    print("-" * 30)
     print(f"TOTAL: " + " " * 15 + f"${total_price:.2f}")
 
 bookstore_checkout()
